27b7fb3e19ac5608e5ed9ba6fc02af51def70cce
Plofland <ploflan@gmail.com>
2024-03-05 22:51:34 -0500

    add responsive masonry

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,23 @@
 import Image from 'next/image';
 import styles from './page.module.css';
 import styled from 'styled-components';
-import Masonry from 'react-responsive-masonry';
+import Masonry, {
+	ResponsiveMasonry
+} from 'react-responsive-masonry';
 import marcParkPhoto from 'public/photos/18.jpg';
 
 export default function Home() {
 	return (
 		<main className={styles.main}>
 			<MasonGrid>
-
-					<Masonry columnsCount={2} gutter='24px'>
+				<ResponsiveMasonry
+					columnsCountBreakPoints={{
+						350: 1,
+						750: 2,
+						900: 3
+					}}
+				>
+					<Masonry gutter="34px">
 						<ImageCard>
 							{/* <Image
 								src={marcParkPhoto}
@@ -23,26 +31,26 @@ export default function Home() {
 								alt="test photo"
 							></Image> */}
 						</ImageCard>
-            <ImageCard />
-            <ImageCard />
-            <ImageCard />
-            <ImageCard />
+						<ImageCard />
+						<ImageCard />
+						<ImageCard />
+						<ImageCard />
 					</Masonry>
-
+				</ResponsiveMasonry>
 			</MasonGrid>
 		</main>
 	);
 }
 
 const MasonGrid = styled.div`
-  width: 66vw;
-  display: flex;
-  flex-direction: end;
+	width: 66vw;
+	display: flex;
+	flex-direction: end;
 `;
 
 const ImageCard = styled.div`
 	border: 1px solid blue;
 	width: 500px;
 	height: 500px;
-  background-color: salmon;
+	background-color: salmon;
 `;
