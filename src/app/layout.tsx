7aeed5bcf06562a4a30c0915809a77370ef2f122
Plofland <ploflan@gmail.com>
2024-03-28 10:24:31 -0400

    remove stray S string

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
-import StyledComponentsRegistry from '../lib/registry'
+import StyledComponentsRegistry from '../lib/registry';
 import Navbar from '@/components/Navbar';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -20,12 +20,10 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<body className={inter.className}>
-        
 				<StyledComponentsRegistry>
 					<Navbar />
 					{children}
 				</StyledComponentsRegistry>
-				S
 			</body>
 		</html>
 	);
