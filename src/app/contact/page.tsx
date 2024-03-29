b96a14cd7f168c1a096198ecfe55f32e8dc42c23
Plofland <ploflan@gmail.com>
2024-03-29 14:05:58 -0400

    add contact links

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,10 +1,36 @@
 'use client';
+import Link from 'next/link';
 import React from 'react';
 import styled from 'styled-components';
 
 const Contact = () => {
 	return (
-		<ContactContainer>Contact - WIP</ContactContainer>
+		<ContactContainer>
+			<SocialLink
+				href="https://github.com/Plofland"
+				target="_blank"
+			>
+				GitHub
+			</SocialLink>
+			<SocialLink
+				href="https://www.linkedin.com/in/peter-lofland/"
+				target="_blank"
+			>
+				LinkedIn
+			</SocialLink>
+			<SocialLink
+				href="mailto:ploflan@gmail.com"
+				target="_blank"
+			>
+				Email
+			</SocialLink>
+			<SocialLink
+				href="https://www.instagram.com/peterlofland/"
+				target="_blank"
+			>
+				Instagram
+			</SocialLink>
+		</ContactContainer>
 	);
 };
 
@@ -12,5 +38,13 @@ export default Contact;
 
 const ContactContainer = styled.div`
 	display: flex;
-	justify-content: center;
+	width: 100vw;
+	align-items: center;
+	flex-direction: column;
+`;
+
+const SocialLink = styled.a`
+	font-family: 'CeraPro', sans-serif;
+	font-size: 24px;
+	margin: 1em 0 1em 0;
 `;
