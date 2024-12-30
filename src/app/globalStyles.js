46de9cc4b841906a1570d808828f92b4165f8762
Plofland <ploflan@gmail.com>
2024-12-30 11:34:38 -0500

    remove needless boilerplate css, remove ripple types, add styled-components globals

---
+++ src/app/globalStyles.js
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    padding-right: 0;
+    box-sizing: border-box;
+    font-family: 'Garamond', serif;;
+    background-color: #fefefe;
+  }
+`;
+
+export default GlobalStyles;
