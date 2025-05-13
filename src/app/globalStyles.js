d47d81625f1d1b7ba66c0b7ba5b5928f49ee6a63
Plofland <ploflan@gmail.com>
2025-05-13 18:00:35 -0400

    add brick bg

--- src/app/globalStyles.js
+++ src/app/globalStyles.js
@@ -7,8 +7,12 @@ const GlobalStyles = createGlobalStyle`
     padding-right: 0;
     box-sizing: border-box;
     font-family: 'Garamond', serif;;
-    background-color: #080808;
-    height: 100%;
+    background-image: url('/photos/brickBG.jpg');
+    background-size: cover;
+    background-position: center;  
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    height: 100vh;
   }
 `;
 
