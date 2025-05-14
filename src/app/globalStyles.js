26a6c3918993c3ea63d718914eee563a7f5712c9
Plofland <ploflan@gmail.com>
2025-05-14 12:12:04 -0400

    move bg to layout, should fix iOS mobile

--- src/app/globalStyles.js
+++ src/app/globalStyles.js
@@ -7,12 +7,6 @@ const GlobalStyles = createGlobalStyle`
     padding-right: 0;
     box-sizing: border-box;
     font-family: 'Garamond', serif;;
-    background-image: url('/photos/brickBG.jpg');
-    background-size: cover;
-    background-position: center;  
-    background-repeat: no-repeat;
-    background-attachment: fixed;
-    height: 100vh;
   }
 `;
 
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 'use client';
 
+import styled from 'styled-components';
 import GlobalStyles from './globalStyles';
 import CombinedNavMenus from './components/CombinedNavMenus';
 
@@ -8,9 +9,23 @@ export default function RootLayout({ children }) {
 		<html lang="en">
 			<body>
 				<GlobalStyles />
+				<BackgroundImage />
 				<CombinedNavMenus />
 				<main>{children}</main>
 			</body>
 		</html>
 	);
 }
+
+const BackgroundImage = styled.div`
+	position: fixed;
+	top: 0;
+	left: 0;
+	width: 100vw;
+	height: 100vh;
+	background-image: url('/photos/brickBG.jpg');
+	background-size: cover;
+	background-position: center;
+	background-repeat: no-repeat;
+	z-index: -1;
+`;
